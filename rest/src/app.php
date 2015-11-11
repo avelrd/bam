1cad114b06b0890ed47b0e26df1dab78bfc94c90
robert <robert@kleinpenning.com>
2015-11-11 16:54:09 +0100

    list events

--- rest/src/app.php
+++ rest/src/app.php
@@ -31,9 +31,45 @@ $app->get('/events', function () use ($app, $em) {
     return new JsonResponse($encoded);
 });
 
+
 $app->post('/events/add', function (Request $request) use ($app, $em) {
 
     $event = new Event();
+    $event->setStartdate($request->get('startdate', null));
+    $event->setEnddate($request->get('enddate', null));
+    $event->setUsers($request->get('users', []));
+    $event->setLocation($request->get('location', ''));
+    $event->setDescription($request->get('description', ''));
+    $event->setName($request->get('name', null));
+    $event->setHosts($request->get('hosts', []));
+    $event->setTags($request->get('tags', []));
+
+    if($event->getName() === null) {
+        return new JsonResponse(['error'=>'No name set'], 500);
+    }
+
+    $em->persist($event);
+    $em->flush();
+
+    return new JsonResponse(['success'=>'Event stored']);
+
+});
+//$app->get('/events/get/{id}', function ($id) use ($app, $em) {
+//
+//    $event = $em->getRepository('Event')->find($id);
+//    return new JsonResponse(json_encode_event($event));
+//
+//});
+
+$app->get('/events/get/{name}', function ($name) use ($app, $em) {
+    $event = $em->getRepository('Event')->findOneByName($name);
+    return new JsonResponse(json_encode_event($event));
+
+});
+
+$app->post('/events/update/{name}', function ($name, Request $request) use ($app, $em) {
+
+    $event = $em->getRepository('Event')->findOneByName($name);
     $event->setStartdate($request->get('startdate', null));
     $event->setEnddate($request->get('enddate', null));
     $event->setUsers($request->get('users', []));
